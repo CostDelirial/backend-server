38ac2be3eae8a0d7a9a7ea71b55a7ae79673e1e9
CostDelirial <warsystem@MacBook-Pro-de-Gaby.local>
2018-06-17 01:16:23 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 // Requires importacion de librerias 
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 //Inicializacion de variables
 var app = express();
+
+
+//body-parser
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/HospitalDB', (err, res) => {
@@ -14,15 +21,17 @@ mongoose.connection.openUri('mongodb://localhost:27017/HospitalDB', (err, res) =
     console.log('Base de datos: \x1b[32m%s\x1b[0m', ' online');
 });
 
-//rutas
-app.get('/', (req, res, next) => {
+// IMportar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
 
-});
+// Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
+
 
 //Escuchar peticiones
 app.listen(3000, function() {
